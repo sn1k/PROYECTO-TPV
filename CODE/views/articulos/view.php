d9e1baa17ef83883335b3728280aa34d2f7afe3e
Alberto Romero Cañadas <albertoromeroca@gmail.com>
2016-02-13 11:54:43 +0100

    update view/articulo/index with css

--- CODE/views/articulos/view.php
+++ CODE/views/articulos/view.php
@@ -8,30 +8,27 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<link href="<? echo base_url();?>/css/style.css" rel="stylesheet" type="text/css" />
 </head>
   <body>
-
         <div id="container">
-		        	<h1>PROYECTO TPV</h1>
-		        <div id="textblock">
-								<h2>Listado de artículos</h2>
-									<?php foreach ($articulos as $articulos_item): ?>
-											<div class="main">
-									<p><strong><?php echo $articulos_item->nombre; ?></strong></p>
-  										<p><?php echo $articulos_item->descripcion; ?></p>
-				        	</div>
+	     	<h1>PROYECTO TPV</h1>
+	        <div id="textblock">
+			<h2>Listado de artículos</h2>
+			<?php foreach ($articulos as $articulos_item): ?>
+				<div class="main">
+				<p><strong><?php echo $articulos_item->nombre; ?></strong></p>
+				<p><?php echo $articulos_item->descripcion; ?></p>
+		        	</div>
 
-									<a href="<?php echo site_url('articulos/view/'.$articulos_item->idArticulo); ?>"><IMG  SRC="<? echo base_url();?>/css/img/Eye.png" width="12" height="12">Ver artículo</a><br/>
-									<a href="<?php echo site_url('carritos/create/'.$articulos_item->idArticulo); ?>"><IMG  SRC="<? echo base_url();?>/css/img/add-icon.png" width="12" height="12">Añadir carrito</a>
-									<?php endforeach; ?>
+				<a href="<?php echo site_url('articulos/view/'.$articulos_item->idArticulo); ?>"><IMG  SRC="<? echo base_url();?>/css/img/Eye.png" width="12" height="12">Ver artículo</a><br/>
+				<a href="<?php echo site_url('carritos/create/'.$articulos_item->idArticulo); ?>"><IMG  SRC="<? echo base_url();?>/css/img/add-icon.png" width="12" height="12">Añadir carrito</a>
+			<?php endforeach; ?>
+				</div>
+				<div id="addandticket">
+					<p><a href="<?php echo site_url('articulos/create'); ?>"><IMG  SRC="<? echo base_url();?>/css/img/add.png" width="12" height="12">Añadir artículo</a><br/>
+					<a href="<?php echo site_url('tickets/'); ?>"><IMG  SRC="<? echo base_url();?>/css/img/carro.png" width="12" height="12">Tickets</a></p>
+				</div>
 
-
-						</div>
-							<div id="addandticket">
-									<p><a href="<?php echo site_url('articulos/create'); ?>"><IMG  SRC="<? echo base_url();?>/css/img/add.png" width="12" height="12">Añadir artículo</a><br/>
-									<a href="<?php echo site_url('tickets/'); ?>"><IMG  SRC="<? echo base_url();?>/css/img/carro.png" width="12" height="12">Tickets</a></p>
-							</div>
-
-			        <p class="footer"><A HREF="https://github.com/sn1k/PROYECTO-TPV"><IMG id="github" SRC="<? echo base_url();?>/css/img/github.png" width="16" height="16">Este proyecto en GitHub</A><br/>Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
-	      </div>
+		 <p class="footer"><A HREF="https://github.com/sn1k/PROYECTO-TPV"><IMG id="github" SRC="<? echo base_url();?>/css/img/github.png" width="16" height="16">Este proyecto en GitHub</A><br/>Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+	 </div>
 
     </body>
 </html>
