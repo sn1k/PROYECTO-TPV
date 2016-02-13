bd00a27b3ac4a57976a451c4de5e69b0dd2b850a
sn1k <albertoromero@gmail.com>
2016-02-13 12:00:06 +0100

    full project upload 3

--- FULL PROJECT/application/views/articulos/index.php
+++ FULL PROJECT/application/views/articulos/index.php
@@ -6,91 +6,33 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
 	<meta charset="utf-8">
 	<title>TPV - ALBERTO ROMERO</title>
-
-	<style type="text/css">
-
-	::selection { background-color: #E13300; color: white; }
-	::-moz-selection { background-color: #E13300; color: white; }
-
-	body {
-		background-color: #fff;
-		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
-		color: #4F5155;
-	}
-
-	a {
-		color: #003399;
-		background-color: transparent;
-		font-weight: normal;
-	}
-
-	h1 {
-		color: #444;
-		background-color: transparent;
-		border-bottom: 1px solid #D0D0D0;
-		font-size: 19px;
-		font-weight: normal;
-		margin: 0 0 14px 0;
-		padding: 14px 15px 10px 15px;
-	}
-
-	code {
-		font-family: Consolas, Monaco, Courier New, Courier, monospace;
-		font-size: 12px;
-		background-color: #f9f9f9;
-		border: 1px solid #D0D0D0;
-		color: #002166;
-		display: block;
-		margin: 14px 0 14px 0;
-		padding: 12px 10px 12px 10px;
-	}
-
-	#body {
-		margin: 0 15px 0 15px;
-	}
-
-	p.footer {
-		text-align: right;
-		font-size: 11px;
-		border-top: 1px solid #D0D0D0;
-		line-height: 32px;
-		padding: 0 10px 0 10px;
-		margin: 20px 0 0 0;
-	}
-
-	#container {
-		margin: 10px;
-		border: 1px solid #D0D0D0;
-		box-shadow: 0 0 8px #D0D0D0;
-	}
-	</style>
+	<link href="<? echo base_url();?>/css/style.css" rel="stylesheet" type="text/css" />
 </head>
   <body>
 
         <div id="container">
-        	<h1>PROYECTO TPV</h1>
-        <div>
-					<h2>Listado de artículos</h2>
-<?php foreach ($articulos as $articulos_item): ?>
-        <h3><?php echo $articulos_item->nombre; ?></h3>
-        <div class="main">
-                <?php echo $articulos_item->descripcion; ?>
-        </div>
-        <p>
-					<a href="<?php echo site_url('articulos/view/'.$articulos_item->idArticulo); ?>">Ver artículo</a>
-					<a href="<?php echo site_url('carritos/create/'.$articulos_item->idArticulo); ?>">Añadir carrito</a>
+		        	<h1>PROYECTO TPV</h1>
+		        <div id="textblock">
+								<h2>Listado de artículos</h2>
+									<?php foreach ($articulos as $articulos_item): ?>
+											<div class="main">
+									<p><strong><?php echo $articulos_item->nombre; ?></strong></p>
+  										<p><?php echo $articulos_item->descripcion; ?></p>
+				        	</div>
 
-				</p>
+									<a href="<?php echo site_url('articulos/view/'.$articulos_item->idArticulo); ?>"><IMG  SRC="<? echo base_url();?>/css/img/Eye.png" width="12" height="12">Ver artículo</a><br/>
+									<a href="<?php echo site_url('carritos/create/'.$articulos_item->idArticulo); ?>"><IMG  SRC="<? echo base_url();?>/css/img/add-icon.png" width="12" height="12">Añadir carrito</a>
+									<?php endforeach; ?>
 
-<?php endforeach; ?>
-        </div>
 
-				<p><a href="<?php echo site_url('articulos/create'); ?>">Añadir artículo</a></p>
-				<p><a href="<?php echo site_url('tickets/'); ?>">Tickets</a></p>
+						</div>
+							<div id="addandticket">
+									<p><a href="<?php echo site_url('articulos/create'); ?>"><IMG  SRC="<? echo base_url();?>/css/img/add.png" width="12" height="12">Añadir artículo</a><br/>
+									<a href="<?php echo site_url('tickets/'); ?>"><IMG  SRC="<? echo base_url();?>/css/img/carro.png" width="12" height="12">Tickets</a></p>
+							</div>
 
-        	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
-        </div>
+			        <p class="footer"><A HREF="https://github.com/sn1k/PROYECTO-TPV"><IMG id="github" SRC="<? echo base_url();?>/css/img/github.png" width="16" height="16">Este proyecto en GitHub</A><br/>Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+	      </div>
 
     </body>
 </html>
